4940e92ef0c9569a069d054bf48973c803b5dfbf
gotoinc <root@gotoinc.co>
2021-10-15 14:52:53 +0300

    styling/feature//made popap

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -117,3 +117,14 @@ function Cart() {
 }
 
 export default Cart;
+
+
+<div className="prompt">
+		<div className="prompt__wrapper">
+			<h2 className="prompt__title">Вы действительно хотите удалить?</h2>
+			<div className="prompt__buttons">
+				<button type="button" className="prompt__btn prompt__btn_cancel">Отменить</button>
+				<button type="button" className="prompt__btn prompt__btn_okay">Да</button>
+			</div>
+		</div>
+</div>
