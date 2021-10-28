c2dbdc36e7108f062e63d29355920e78f8e8f039
gotoinc <root@gotoinc.co>
2021-10-28 18:12:27 +0300

    styling/refactor/moved prompt to a separate component and fixed prompt styles

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,6 +7,7 @@ import CartItem from '../components/CartItem';
 import Button from '../components/Button';
 
 import cartEmptyImage from '../assets/img/empty-cart.png'
+import Prompt from '../components/Prompt';
 
 function Cart() {
 
@@ -52,9 +53,9 @@ function Cart() {
                   <h2 className="content__title">
                     Корзина</h2>
                   <div className="cart__clear">
-                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" clip-rule="evenodd" d="M13.7787 2.46466L12.1624 0.848417L7.31372 5.69711L2.46502 0.848417L0.84878 2.46466L5.69747 7.31336L0.848711 12.1621L2.46495 13.7784L7.31372 8.92961L12.1625 13.7784L13.7787 12.1621L8.92996 7.31336L13.7787 2.46466Z" fill="#8B949D"/>
-                  </svg>
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M13.7787 2.46466L12.1624 0.848417L7.31372 5.69711L2.46502 0.848417L0.84878 2.46466L5.69747 7.31336L0.848711 12.1621L2.46495 13.7784L7.31372 8.92961L12.1625 13.7784L13.7787 12.1621L8.92996 7.31336L13.7787 2.46466Z" fill="#8B949D" />
+                    </svg>
                     <span onClick={onClearCart}>Очистить корзину</span>
                   </div>
                 </div>
@@ -86,9 +87,9 @@ function Cart() {
                   </div>
                   <div className="cart__bottom-buttons">
                     <Link to="/" className="button button--outline button--add go-back-btn">
-                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 10.9288L1 6.23187L5.88479 1.64307" stroke="#151515" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 10.9288L1 6.23187L5.88479 1.64307" stroke="#151515" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                      </svg>
 
 
                       <span>Назад</span>
@@ -117,14 +118,3 @@ function Cart() {
 }
 
 export default Cart;
-
-
-<div className="prompt">
-		<div className="prompt__wrapper">
-			<h2 className="prompt__title">Вы действительно хотите удалить?</h2>
-			<div className="prompt__buttons">
-				<button type="button" className="prompt__btn prompt__btn_cancel">Отменить</button>
-				<button type="button" className="prompt__btn prompt__btn_okay">Да</button>
-			</div>
-		</div>
-</div>
