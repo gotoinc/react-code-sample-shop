7d14aae4b65c4b4064ba5e002df6253df8be297e
gotoinc <root@gotoinc.co>
2021-10-29 15:14:21 +0300

    styling/refactor/change prompt to confirm and add confirm logic

---
+++ src/components/Confirm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Confirm({ question, onConfirmClick }) {
+   return (
+      <div className="confirm">
+         <div className="confirm__wrapper">
+            <h2 className="confirm__title">{question}</h2>
+            <div className="confirm__buttons">
+               <button onClick={() => onConfirmClick(false)} type="button" className="confirm__btn confirm__btn_cancel">Отменить</button>
+               <button onClick={() => onConfirmClick(true)} type="button" className="confirm__btn confirm__btn_okay">Да</button>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Confirm
