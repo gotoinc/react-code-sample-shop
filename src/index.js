c33e04771b20740eedf5f3618b2afce25cca6bd3
gotoinc <root@gotoinc.co>
2021-11-08 17:06:37 +0200

    changed browserrouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { HashRouter as Router, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
