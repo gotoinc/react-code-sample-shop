6ed6a470127142ae5e3e1dd3285a8e6208b3e221
Vadym Kononenko <vadymk95@gmail.com>
2021-11-12 16:26:21 +0200

    5/tests/confirm-component-test

---
+++ src/__tests__/Confirm.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Confirm from '../components/Confirm';
+import { act } from 'react-dom/test-utils';
+
+const title = 'Hello World!';
+
+describe('Confirm', () => {
+  describe('renders Confirm component', () => {
+    it('has the buttons', () => {
+      render(<Confirm />);
+      expect(screen.getByText('Cancel')).toBeInTheDocument();
+      expect(screen.getByText('Yes')).toBeInTheDocument();
+    });
+
+    it('has a title', () => {
+      render(<Confirm question={title} />);
+      expect(screen.getByText(title)).toBeInTheDocument();
+    });
+  });
+
+  describe('click on Confirm buttons', () => {
+    it('click on button', () => {
+      
+    });
+  });
+});
