27882c7dee8035e85b3676cacd1650c65739a947
Gotoinc <root@gotoinc.co>
2021-11-19 18:43:11 +0200

    5/tests/prepared tests environment for Home page

--- src/__tests__/Confirm.test.jsx
+++ src/__tests__/Confirm.test.jsx
@@ -7,22 +7,27 @@ const title = 'Hello World!';
 describe('Confirm', () => {
   it('renders Confirm component', () => {
     render(<Confirm question={title} />);
+
     expect(screen.getByText(title)).toBeInTheDocument();
   });
 
   it('should click the cancel button', () => {
     const mockConfirmAction = jest.fn();
     render(<Confirm onConfirmClick={mockConfirmAction} />);
+
     const button = screen.getByTestId('cancel-button');
     fireEvent.click(button);
+
     expect(mockConfirmAction).toHaveBeenCalled();
   });
 
   it('should click the confirm button', () => {
     const mockConfirmAction = jest.fn();
     render(<Confirm onConfirmClick={mockConfirmAction} />);
+
     const button = screen.getByTestId('confirm-button');
     fireEvent.click(button);
+
     expect(mockConfirmAction).toHaveBeenCalled();
   });
 });
