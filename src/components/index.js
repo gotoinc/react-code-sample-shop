4426880be82a9db160dc9993b4712ff1b1dc4406
Gotoinc <root@gotoinc.co>
2021-11-21 21:37:12 +0200

    5/tests/added-test-to-Cart-page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as Button } from './Button';
+export { default as CartItem } from './CartItem';
+export { default as Confirm } from './Confirm';
 export { default as Header } from './Header';
 export { default as Categories } from './Categories';
 export { default as SortPopup } from './SortPopup';
